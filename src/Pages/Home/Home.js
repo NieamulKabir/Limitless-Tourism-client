5caa0785d0c0b51ee141a60f24f94642915037a6
NieamulKabir <kabir000079@gmail.com>
2022-10-05 20:50:01 +0600

    add core values

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CoreValue from './CoreValue/CoreValue';
 import HeroSection from './HeroSection/HeroSection';
 
 const Home = () => {
     return (
         <div>
             <HeroSection></HeroSection>
+            <CoreValue></CoreValue>
         </div>
     );
 };
